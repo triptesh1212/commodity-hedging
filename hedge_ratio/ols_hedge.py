fe6912c1e9fad4ab46483c47888745ed8549fd61
triptesh1212 <biswastriptesh@gmail.com>
2025-09-04 09:21:04 +0530

    code fix

--- hedge_ratio/ols_hedge.py
+++ hedge_ratio/ols_hedge.py
@@ -9,7 +9,6 @@ def max_drawdown(returns):
     return drawdown.min()
 
 def compute_ols_hedge():
-    
     df = pd.read_csv("dataset/brent_spot_futures.csv")
     df['Date'] = pd.to_datetime(df['Date'])
     df = df.set_index('Date')
@@ -18,34 +17,33 @@ def compute_ols_hedge():
     df['Fut_ret'] = np.log(df['Futures'] / df['Futures'].shift(1))
     df = df.dropna()
 
+    # Train : 2018–2023
+    # Test : 2024
+
+    train = df.loc["2018":"2023"].copy()
+    test = df.loc["2024"].copy()
+
     # OLS regression: Spot_ret ~ Fut_ret
+    X_train = sm.add_constant(train['Fut_ret'])
+    model = sm.OLS(train['Spot_ret'], X_train).fit()
+    _, beta = model.params
 
-    X = sm.add_constant(df['Fut_ret'])
-    model = sm.OLS(df['Spot_ret'], X).fit()
-    
-    alpha, beta = model.params
+    test['Hedged_ret'] = test['Spot_ret'] - beta * test['Fut_ret']
 
-    df['Hedged_ret'] = df['Spot_ret'] - beta * df['Fut_ret']
-
-    var_spot = df['Spot_ret'].var()
-    var_hedged = df['Hedged_ret'].var()
+    var_spot = test['Spot_ret'].var()
+    var_hedged = test['Hedged_ret'].var()
     he_ols = 1 - (var_hedged / var_spot)
 
-    mean_hedged = df['Hedged_ret'].mean()
-    vol_hedged = df['Hedged_ret'].std()
+    mean_hedged = test['Hedged_ret'].mean()
+    vol_hedged = test['Hedged_ret'].std()
 
-    rf = 0 # risk-free rate
+    sharpe_hedged = (mean_hedged / vol_hedged) * np.sqrt(252) if vol_hedged > 0 else np.nan
+    mdd_hedged = max_drawdown(test['Hedged_ret'])
 
-    sharpe_hedged = ((mean_hedged - rf) / vol_hedged) * np.sqrt(252)
+    print("\n<--------- OLS Hedge---------->\n")
 
-    mdd_hedged = max_drawdown(df['Hedged_ret'])
-
-    print("<--------- OLS Hedge ---------->")
-    print("")
-   
     print(f"Variance of OLS Hedged Portfolio: {var_hedged:.6f}")
     print(f"Hedge Effectiveness (OLS Hedge): {he_ols * 100:.6f}%")
     print(f"Sharpe Ratio (OLS Hedged): {sharpe_hedged:.6f}")
-    print(f"Max Drawdown (OLS Hedged): {mdd_hedged * 100:.6f}%")
+    print(f"Max Drawdown (OLS Hedged): {mdd_hedged * 100:.6f}%\n")
 
-    print("")
